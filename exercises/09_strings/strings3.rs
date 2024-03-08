367918708c0d3d8a47c862a7fd309c705624b0ae
Hoang Van Loc <loc.hoang@olli-ai.com>
2024-03-08 16:15:32 +0700

    Excercise backup

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let new_str: String = String::from(input);
+    String::from(new_str.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut new_str: String = String::from(input);
+    new_str.push_str(" world!");
+    new_str
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut new_str: String = String::from(input);
+    String::from(new_str.replace("cars", "balloons"))
 }
 
 #[cfg(test)]
